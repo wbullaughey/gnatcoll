5322f38bd83fdadcba29f94f01f7f01fb3f14e27
wbullaughey <wlb122@verizon.net>
2025-06-24 18:42:42 -0400

    merge to desktop 6/24/25

--- check_out.sh
+++ check_out.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
-COMMENT=$1
-if [[ -z "$COMMENT" ]]; then
-   echo no comment suppled
-else
-   function pull(){
-      MODULE=$1
-      if [[ -d "$MODULE" ]]; then
-         pushd $MODULE >/dev/null 2>&1
-         if [[ $? -eq 0 ]]; then
-            echo "pull $MODULE with comment $COMMENT"
-            git pull
-            popd >/dev/null 2>&1
-         else
-             echo "pushd failed"
-             exit
-         fi
+
+function pull(){
+   MODULE=$1
+   if [[ -d "$MODULE" ]]; then
+      pushd $MODULE >/dev/null 2>&1
+      if [[ $? -eq 0 ]]; then
+         echo "pull $MODULE"
+         git pull
+         popd >/dev/null 2>&1
       else
-          echo "Directory $MODULE does not exist"
+          echo "pushd failed"
           exit
       fi
-   }
+   else
+       echo "Directory $MODULE does not exist"
+       exit
+   fi
+}
 
-   pull "ada_lib"
-   pull "ada_lib/ada_lib_test_lib"
-   pull "ada_lib/ada_lib_tests"
-   pull "applications"
-   pull "aunit"
-   pull "gnoga_lib"
-   pull "vendor/github.com/gnoga"
-   pull "."
-fi
+pull "ada_lib"
+pull "ada_lib/ada_lib_test_lib"
+pull "ada_lib/ada_lib_tests"
+pull "applications"
+pull "aunit"
+pull "gnoga_lib"
+pull "vendor/github.com/gnoga"
+pull "."
 
