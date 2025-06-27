dbe226481c89796afb23acda1210312a98bdc7b2
wbullaughey <wlb122@verizon.net>
2025-06-27 13:54:51 -0400

    temp branch changes 6-27-25

--- fix_alire_toml.sh
+++ fix_alire_toml.sh
@@ -2,9 +2,11 @@
 SOURCE=$1
 TARGET=alire.toml
 
+# must be run from directory where alire.toml.source exists
+
 if [ -f "$SOURCE" ]; then
    echo "$SOURCE exists"
-   echo fixing $SOURCE creating $TARGET
+   echo fixing $SOURCE creating $TARGET for $ALR_BUILD_ENVIRONMENT
 
    case $ALR_BUILD_ENVIRONMENT in
 
@@ -18,6 +20,10 @@ if [ -f "$SOURCE" ]; then
 
       "*")
          echo invalid ALR_BUILD_ENVIRONMENT value /$ALR_BUILD_ENVIRONMENT/
+         ;;
+
+      "")
+         echo missing ALR_BUILD_ENVIRONMENT
          ;;
    esac
 else
